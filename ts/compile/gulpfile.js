966dda3a9897a1912239e2a9d5e67cb9988431d2
Philipp Kunz <kunzphp@gmail.com>
2015-12-02 21:20:52 +0100

    update npm scripts

--- ts/compile/gulpfile.js
+++ ts/compile/gulpfile.js
@@ -1,6 +1,6 @@
 // import gulp
-var gulp = require("gulp"),
-	gulpTypescript = require("gulp-typescript");
+var gulp = require("gulp");
+var gulpTypescript = require("gulp-typescript");
 
 gulp.task('compileTS', function() {
 	var stream = gulp.src('../index.ts')
@@ -14,3 +14,4 @@ gulp.task('compileTS', function() {
 gulp.task('default',['compileTS'], function() {
 	console.log('Typescript compiled');
 });
+gulp.start.apply(gulp, ['default']);
